ad84d21aa9d80773c5cfa8055c8f08370953be6c
eddy08 <bhardwajharsh08@gmail.com>
2023-03-26 22:39:08 +0530

    🥇 Initial Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+### Neo4j Schema and Corresponding Data
+
+[neo4j](./neo4j-parent-child.png)
+
+### Groovy Example for Github Issue
+
+1. Groovy Tryout for issue : https://github.com/neo4j-graphql/neo4j-graphql-java/issues/277
+2. To run the code try out the gitpod button
+
+### Commands to run Neo4j container
+
+docker run \
+ --publish=7474:7474 --publish=7687:7687 \
+ --volume=$HOME/neo4j/data:/data \
+ neo4j
+
+### Neo4j Script:
+
+CREATE(p1:Parent {parent_id:'p1',parent_name:'P1'}),
+(p2:Parent {parent_id:'p2',parent_name:'P2'}),
+
+        (c1:Child {child_id:'c1'}),
+        (c2:Child {child_id:'c2'}),
+
+        (p1)-[:Parent_Child_Mapping{edgeDetails:'edge_1'}]->(c1),
+        (p1)-[:Parent_Child_Mapping{edgeDetails:'edge_2'}]->(c1),
+        (p1)-[:Parent_Child_Mapping{edgeDetails:'edge_3'}]->(c1),
+        (p2)-[:Parent_Child_Mapping{edgeDetails:'edge_1'}]->(c2),
+        (p2)-[:Parent_Child_Mapping{edgeDetails:'edge_2'}]->(c2),
+        (p2)-[:Parent_Child_Mapping{edgeDetails:'edge_3'}]->(c2)
---
+++ neo4j-query.groovy
@@ -0,0 +1,42 @@
+// Simplistic GraphQL Server using SparkJava
+@Grapes([
+  @Grab('com.sparkjava:spark-core:2.7.2'),
+  @Grab('org.neo4j.driver:neo4j-java-driver:1.7.2'),
+  @Grab('com.google.code.gson:gson:2.8.5'),
+  @Grab('org.neo4j:neo4j-graphql-java:1.7.0')
+])
+
+import spark.*
+import static spark.Spark.*
+import com.google.gson.Gson
+import org.neo4j.graphql.*
+import org.neo4j.driver.v1.*
+
+schema = """
+type Person {
+  name: ID!
+  born: Int
+  actedIn: [Movie] @relation(name:"ACTED_IN")
+}
+type Movie {
+  title: ID!
+  released: Int
+  tagline: String
+}
+type Query {
+    person : [Person]
+}
+"""
+
+gson = new Gson()
+render = (ResponseTransformer)gson.&toJson
+def query(value) { gson.fromJson(value,Map.class)["query"] }
+
+graphql = new Translator(SchemaBuilder.buildSchema(schema))
+def translate(query) { graphql.translate(query) }
+
+driver = GraphDatabase.driver("bolt://localhost",AuthTokens.basic("neo4j","s3cr3t"))
+def run(cypher) { driver.session().withCloseable {
+     it.run(cypher.query, Values.value(cypher.params)).list{ it.asMap() }}}
+
+post("/graphql","application/json", { req, res ->  run(translate(query(req.body())).first()) }, render);
