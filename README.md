5045d5aee55e2a88aa323e640c6535f1cce35f0d
eddy08 <bhardwajharsh08@gmail.com>
2023-03-27 22:47:05 +0530

    Updated groovy script 📜

--- README.md
+++ README.md
@@ -1,12 +1,47 @@
 ### Neo4j Schema and Corresponding Data
 
+Schema:
+
+```
+type Parent{
+     parent_id: String
+     parent_name: String
+    Parent_Child_Mapping:[Parent_Child_Mapping]
+}
+type Parent_Child_Mapping @relation (name: "Parent_Child_Mapping", from :"Parent", to: "Child"){
+     Parent: [Parent]
+    Child: [Child]
+    edgeDetails: String
+}
+type Child{
+     child_id: String
+     Parent_Child_Mapping: [Parent_Child_Mapping]
+}
+```
+
 ![neo4j](./neo4j-parent-child.png)
+
+Query ( TO RUN ):
+
+```
+           Parent(first:2) {
+                    parent_id
+
+                    Parent_Child_Mapping(first:2){
+                        edgeDetails
+
+                        Child(first:2){
+                                    child_id
+                                    }
+                            }
+                    }
+```
 
 ### Groovy Example for Github Issue
 
 1. Groovy Tryout for issue : https://github.com/neo4j-graphql/neo4j-graphql-java/issues/277
 2. To run the code try out the gitpod button
-
+3. ─➤ groovy -Dgroovy.grape.report.downloads=true -Divy.message.logger.level=4 neo4j-query.groovy
 ### Commands to run Neo4j container
 
 $/home/others/groovy$ docker run --publish=7474:7474 --publish=7687:7687 neo4j:5.6.0-community
@@ -25,3 +60,4 @@ CREATE(p1:Parent {parent_id:'p1',parent_name:'P1'}),
         (p2)-[:Parent_Child_Mapping{edgeDetails:'edge_1'}]->(c2),
         (p2)-[:Parent_Child_Mapping{edgeDetails:'edge_2'}]->(c2),
         (p2)-[:Parent_Child_Mapping{edgeDetails:'edge_3'}]->(c2)
+
--- neo4j-query.groovy
+++ neo4j-query.groovy
@@ -1,7 +1,7 @@
 // Simplistic GraphQL Server using SparkJava
 @Grapes([
   @Grab('com.sparkjava:spark-core:2.7.2'),
-  @Grab('org.neo4j.driver:neo4j-java-driver:1.7.2'),
+  @Grab('org.neo4j.driver:neo4j-java-driver:5.6.0'),
   @Grab('com.google.code.gson:gson:2.8.5'),
   @Grab('org.neo4j:neo4j-graphql-java:1.7.0')
 ])
@@ -10,33 +10,48 @@ import spark.*
 import static spark.Spark.*
 import com.google.gson.Gson
 import org.neo4j.graphql.*
-import org.neo4j.driver.v1.*
+import org.neo4j.driver.*
 
 schema = """
-type Person {
-  name: ID!
-  born: Int
-  actedIn: [Movie] @relation(name:"ACTED_IN")
+type Parent{
+     parent_id: String
+     parent_name: String
+    Parent_Child_Mapping:[Parent_Child_Mapping]
 }
-type Movie {
-  title: ID!
-  released: Int
-  tagline: String
+type Parent_Child_Mapping @relation (name: "Parent_Child_Mapping", from :"Parent", to: "Child"){
+     Parent: [Parent]
+    Child: [Child]
+    edgeDetails: String
+}
+type Child{
+     child_id: String
+     Parent_Child_Mapping: [Parent_Child_Mapping]
 }
 type Query {
-    person : [Person]
-}
+    parent : [Parent]
+    child :  [Child] 
+    }
 """
+print "hello"
+
 
 gson = new Gson()
 render = (ResponseTransformer)gson.&toJson
 def query(value) { gson.fromJson(value,Map.class)["query"] }
 
 graphql = new Translator(SchemaBuilder.buildSchema(schema))
-def translate(query) { graphql.translate(query) }
+def translate(query) {
+     println query
+      graphql.translate(query)
+       }
 
-driver = GraphDatabase.driver("bolt://localhost",AuthTokens.basic("neo4j","s3cr3t"))
+driver = GraphDatabase.driver("bolt://localhost",AuthTokens.basic("neo4j","Harsh@123"))
 def run(cypher) { driver.session().withCloseable {
      it.run(cypher.query, Values.value(cypher.params)).list{ it.asMap() }}}
 
+try{
 post("/graphql","application/json", { req, res ->  run(translate(query(req.body())).first()) }, render);
+}
+catch (Exception e){
+     print e
+}
